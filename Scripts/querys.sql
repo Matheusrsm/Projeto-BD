2dbf9198b96809f4165f24abf38143e9eafe84c4
thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2019-06-23 15:22:16 -0300

    adding query15

--- Scripts/querys.sql
+++ Scripts/querys.sql
@@ -9,4 +9,28 @@ HAVING COUNT(*) >= all
       SELECT COUNT(*)
       FROM MANUTENCAO
       GROUP BY tipo_manutencao  
-    )
+    );
+
+-- Query 15
+-- Crie um trigger que não permita que um cliente se hospede sem reserva.
+
+CREATE OR REPLACE TRIGGER verificaReserva
+BEFORE INSERT ON HOSPEDA
+FOR EACH ROW
+DECLARE
+    v_cpf VARCHAR(14);
+BEGIN
+    
+    SELECT cpf_cliente INTO v_cpf
+    FROM Reserva r
+    WHERE r.cpf_cliente = :new.cpf_cliente AND
+          r.numero_quarto = :new.numero_quarto ;
+    
+    EXCEPTION
+    WHEN no_data_found THEN
+        v_cpf := NULL;
+    
+    IF (v_cpf IS NULL) THEN
+        RAISE_APPLICATION_ERROR (-20202,'Cliente não tem reserva para este quarto, logo, não poderá ser hospedar.');
+    END IF;
+END verificaReserva; 
